083efaa9354d4cc472fd7b46cb5214688fd8c659
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-08-17 12:49:38 +0100

    Fix lint issues

--- server/testutils/factories/ukPostcode.ts
+++ server/testutils/factories/ukPostcode.ts
@@ -2,4 +2,5 @@ import { faker } from '@faker-js/faker/locale/en_GB'
 
 // faker's en_GB zipCode() can emit postcodes that fail our isValidUKPostcode regex
 // this generates one that always matches it.
+// eslint-disable-next-line import/prefer-default-export
 export const ukPostcode = (): string => faker.helpers.fromRegExp(/[A-Z]{1,2}[0-9][0-9A-Z]? [0-9][A-Z]{2}/)
