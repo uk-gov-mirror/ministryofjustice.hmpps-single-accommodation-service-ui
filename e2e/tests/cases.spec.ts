ea461a31b441fa2e6af857f51da1d9387feb890d
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2026-09-23 14:41:09 +0100

    Update case list test to check both tabs

--- e2e/tests/cases.spec.ts
+++ e2e/tests/cases.spec.ts
@@ -11,7 +11,16 @@ test('Case list', async ({ page, users: { probation: probationUser }, cases: { B
   // GIVEN I sign in as a probation user
   await signIn(page, probationUser)
 
-  // AND I can see the expected case
+  // THEN I should see the case list page
+  await expect(page.getByRole('heading', { name: 'Accommodation case list', level: 1 })).toBeVisible()
+
+  // WHEN the expected case is not on the default tab
+  const [expectedForename, expectedSurname] = BASE_CASE.name.split(' ')
+  if (!(await page.getByRole('link', { name: `${expectedSurname}, ${expectedForename}` }).isVisible())) {
+    await page.getByRole('link', { name: 'Settled housing secured' }).click()
+  }
+
+  // THEN I can see the expected case
   const { caseLink, forename, surname } = await getCaseLink(page, BASE_CASE)
 
   // WHEN I click on the case link
